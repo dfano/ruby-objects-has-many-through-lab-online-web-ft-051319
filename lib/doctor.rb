4643054f9e7a443f346f50abc6777a61e7621be6
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-11 17:31:58 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,4 +18,8 @@ class Doctor
     Appointment.all.select { |appointment| appointment.doctor == self }
   end
 
+  def patients #drsteve.patients => [pat1, pat2, pat3]
+    Patient.all.select {|patient| patient.doctor == self}
+  end
+
 end
